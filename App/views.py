29851081565cc98a11a3919b39832b32d6200a44
sayid0927 <13113639501@163.com>
2017-10-04 00:33:49 +0800

    test

--- App/views.py
+++ App/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
 # Create your views here.
+import json
+from django.http import HttpResponse
+
+
+def home(resp):
+    resp = {'errorcode': 100, 'detail': 'Get success'}
+    return HttpResponse(json.dumps(resp), content_type="application/json")
