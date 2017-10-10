f41f655a4b942dd851f91e72abadeeb46509a4d8
sayid0927 <13113639501@163.com>
2017-10-10 22:12:35 +0800

    test

--- App/views.py
+++ App/views.py
@@ -3,7 +3,6 @@ from django.shortcuts import render
 
 # Create your views here.
 import json
-from django.http import HttpResponse, JsonResponse
 from  App.utils import *
 import codecs
 
@@ -11,29 +10,11 @@ def home(resp):
     conn = pymysql.connect(host='127.0.0.1', port=3306, user='sayid', passwd='111', db='test', charset='utf8')
     cursor = conn.cursor()
     cursor.execute("select * from book_list")
-    # re = cursor.fetchall()
-    # fields = cursor.description
     data = dictfetchall(cursor)
-    # column_list = []
-    # for i in fields:
-    #     column_list.append(i[0])
 
-    # with codecs.open('json.txt', 'w+',encoding='utf-8') as f:
-    #     for row in re: # 一次循环，row代表一行，row以元组的形式显示。
-    #         result = {} # 定义Python 字典
-    #         result[column_list[0]] = row[0] # 将row中的每个元素，追加到字典中。　
-    #         result[column_list[1]] = str(row[1])# Python字段格式 和json字段格式转换
-    #         result[column_list[2]] = str(row[2])
-    #         # result[column_list[3]] = row[3]
-    #         # result[column_list[4]] = str(row[4])
-    #         jsondata = json.dumps(result, ensure_ascii=False)  # Python的dict --转换成----> json的object
-    #         f.write(jsondata + '\n')# 写入文件
-    # f.close()
-    resp = {'errorcode': 100, 'detail': 'Get success'}
-    # return HttpResponse(json.dumps(resp), content_type="application/json")
-    # return HttpResponse(json.dumps(resp,ensure_ascii=False), content_type="application/json")
     print(data)
-    return JsonResponse(data, safe=False, json_dumps_params={'ensure_ascii': False})
+
+    # return JsonResponse(data, safe=False, json_dumps_params={'ensure_ascii': False})
 
 
 def dictfetchall(cursor):
