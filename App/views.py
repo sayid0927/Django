22c684f74d6f60291ae913f670a35ee19c9780db
sayid0927 <13113639501@163.com>
2017-10-10 22:22:00 +0800

    test

--- App/views.py
+++ App/views.py
@@ -1,4 +1,5 @@
 # -*- coding:utf-8 -*-
+from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
@@ -9,16 +10,15 @@ import codecs
 def home(resp):
     conn = pymysql.connect(host='127.0.0.1', port=3306, user='sayid', passwd='111', db='test', charset='utf8')
     cursor = conn.cursor()
-    cursor.execute("select * from book_list")
+    cursor.execute("select * from book_name")
     data = dictfetchall(cursor)
-
-    print(data)
-
-    # return JsonResponse(data, safe=False, json_dumps_params={'ensure_ascii': False})
-
+    if len(data) == 0:
+        jsons = json.dumps({"res": '00001', "msg": data})
+    else:
+        jsons = json.dumps({"res": '00000', "msg": data})
+    return HttpResponse(jsons, content_type="application/json")
 
 def dictfetchall(cursor):
-    "Return all rows from a cursor as a dict"
     columns = [col[0] for col in cursor.description]
     return [
         dict(zip(columns, row))
